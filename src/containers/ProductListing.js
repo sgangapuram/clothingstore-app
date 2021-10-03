30257e849062e939fe2dbbf780f6d6bc805fc336
sgangapuram <shrinivas.springs@gmail.com>
2021-10-03 18:19:14 -0500

    Updating complete state with Merging ...prevState as first item in return state object

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,4 +1,4 @@
-import React, {useDebugValue, useEffect, useState} from "react";
+import React, { useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import ProductComponent from "./ProductComponent";
 import axios from "axios";
@@ -8,13 +8,14 @@ const ProductListing = () => {
     const products = useSelector((state) => state.allProducts.products);
     const dispatch = useDispatch();
 
-    // const [count, setCount] = useState(()=>{
-    //     console.log('----->>>>>>Arrow Function version of updating state calls only first time<<<<<<<-----');
-    //     return 4;
-    // });
+    const [state, setCompleteState] = useState(()=>{
+        console.log('----->>>>>>Arrow Function version of updating state calls only first time<<<<<<<-----');
+        return {count:1, theme:'Orange', pokemon:["bulbasaur", "charmander"]};
+    });
 
-    const [count, setCount] = useState(4);
-    const [theme, setTheme] = useState('orange');
+    const count = state.count;
+    const theme = state.theme;
+    const pokemon = state.pokemon;
 
     console.log("%%%%%%%%***products before making axios API call***%%%%%%%%"+products)
     console.log('**********----------default state from app component called----**********');
@@ -37,16 +38,13 @@ const ProductListing = () => {
     console.log("products: ", products);
     //understanding the state update with taking prev state and merging it(...prevState) with specific new fields update
     function incrementCount() {
-        console.log('happy coding');
-        setCount((prevCount)=> prevCount+1);
-        setTheme('Green');
+        setCompleteState((prevState)=>{return {...prevState, count:count+1, pokemon: ["who", "who"], theme:'green'}});
     }
 
     function decrementCountBy2() {
-        console.log('happy world');
-        setCount((prevCount)=> prevCount-1);
-        setTheme('Red');
+        setCompleteState((prevState)=>{return {...prevState, count:count-1, pokemon: ["he", "he"], theme:'red'}});
     }
+
 
     return (
        <>
@@ -54,8 +52,10 @@ const ProductListing = () => {
             <ProductComponent />
             <button onClick={incrementCount}>+</button>
             <div><h2>{count}</h2></div>
+               <div>{pokemon}</div>
             <button onClick={decrementCountBy2}>-</button><br/>
                <span><h1>Total number of products: {products.length}</h1></span>
+               <span><h1>Pokemons total: {pokemon}</h1></span>
            </div>
            <h2>{theme}</h2>
        </>
